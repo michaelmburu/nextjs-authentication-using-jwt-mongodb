9242df0f78ff1d497ebc3150dcc73e6a979aa2f9
Michael Mburu <ichangaimichael@yahoo.com>
2022-12-17 16:54:14 +0300

    nextjs-auth changed client to react and provider to session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { SessionProvider } from 'next-auth/react'
+
+import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/api/signup.js
@@ -0,0 +1,5 @@
+export const handler = () => {
+    
+}
+
+export default handler
